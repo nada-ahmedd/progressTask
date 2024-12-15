108fb3f9bcc66cf170d2342802d9cfd2d67f1dac
nada-ahmedd <nada20-02188@student.eelu.edu.eg>
2024-12-15 18:16:28 +0200

    test

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function startProgress() {
-const progressBar = document.querySelector('.progress-bar');
-const progressText = document.querySelector('.progress-text');
-let progress = 0; 
-const interval = setInterval(() => {
-    if (progress < 100) {
-    progress++; 
-    progressBar.style.width = progress + '%'; 
-    progressText.textContent = progress + '%'; 
+let isLoading = false;
+    document.getElementById('startBtn').addEventListener('click', () => {
+    if (isLoading) return;
+    isLoading = true;
+      const progressBar = document.querySelector('.progress-bar');
+      const progressText = document.querySelector('.progress-text');
+      let progress = 0;
+      const interval = setInterval(() => {
+        if (progress >= 100) {
+          clearInterval(interval);
+          isLoading = false;
         } else {
-    clearInterval(interval); 
+          progress++;
+          progressBar.style.width = progress + '%';
+          progressText.textContent = progress + '%';
         }
-    }, 100); 
-    }
+      }, 100);
+    });
